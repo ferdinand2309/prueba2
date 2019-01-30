0d15f63cce0920241815b2929255a206534df2e5
ferdinand <fernando.olivares23@hotmail.com>
2019-01-29 18:23:54 -0600

    make it better

--- text1/settings.py
+++ text1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'text1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Final2',
+        'USER': 'postgres',
+        'PASSWORD': 'fernando',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -121,3 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
